74e31d0d139710377902a9e8774663c95ff3ca03
Michael Ashley <michaelashley708@gmail.com>
2019-08-02 10:47:37 -0400

    feat: Add basic routing with Login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Would You Rather</div>;
+  return (
+    <div className="app">
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  );
 }
 
 export default App;
